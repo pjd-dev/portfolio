43028b0061a7366813373aaabb54875b9a26f9f7
Darry joachim <contact@darrybook.fr>
2025-12-15 19:01:04 +0100

    podman: add :Z mount to vault volume to fix permission denied

--- podman/run-vault.sh
+++ podman/run-vault.sh
@@ -11,4 +11,6 @@ if [ -f "$ENV_FILE" ]; then
 fi
 
 podman build -t vault -f "$REPO_ROOT/apps/vaulty/Dockerfile" "$REPO_ROOT/apps/vaulty"
-podman run -d --rm --name vaulty --volume "${VAULT_DATA_VOLUME:-vault}":/vault --env-file "$ENV_FILE" vault
+# Use SELinux label :Z on the bind/volume mount to give container access on
+# SELinux-enabled hosts (fixes "Permission denied" when accessing /vault).
+podman run -d --rm --name vaulty --volume "${VAULT_DATA_VOLUME:-vault}":/vault:Z --env-file "$ENV_FILE" vault
